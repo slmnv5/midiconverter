f38b24a3910e7f145bcd834829f14f8366883440
slmnv5 <slmnv5@gmail.com>
2022-01-19 12:47:54 -0500

    1

---
+++ start_test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# prepare all
+aconnect -x
+killall aseqdump
+killall -9 mimap_d
+killall -9 vmpk
+sleep 2
+
+# start this app.
+vmpk &
+sleep 1
+./mimap_d -c count.txt $@  &
+sleep 2
+
+
+CLIENT=$(aconnect -l | awk '/mimap_client/ {print $2}')
+INPORT=$(aconnect -l | awk '/mimap_in_port/ {print $1}')
+OUTPORT=$(aconnect -l | awk '/mimap_out_port/ {print $1}')
+MIDIKEYS=$(aconnect -l | awk '/VMPK Output/ {print $2}')
+echo aconnect ${MIDIKEYS}0 ${CLIENT}${INPORT} 
+aconnect ${MIDIKEYS}0 ${CLIENT}${INPORT}  || exit 1
+
+# see converted messages
+sleep 1
+echo start monitoring MIDI port
+aseqdump -p ${CLIENT}${OUTPORT}
+
+
+
+
