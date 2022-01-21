d96c5b218b27effe80fac993b0a88b257d14661e
slmnv5 <slmnv5@gmail.com>
2022-01-21 15:52:26 -0500

    1

--- start_test.sh
+++ start_test.sh
@@ -7,16 +7,25 @@ killall -9 mimap_d
 killall -9 vmpk
 sleep 2
 
+MIDIKEYS=$(aconnect -l | awk '/BlueBoard/ {print $2;exit}')
+if [ -z "$MIDIKEYS" ]; then 
+	vmpk &
+	sleep 3
+	MIDIKEYS=$(aconnect -l | awk '/VMPK Output/ {print $2;exit}')
+	if [ -z "$MIDIKEYS" ]; then 
+		echo "Could not find or start MIDIKEYS for input"
+		exit 1
+	fi
+fi
+
 # start this app.
 ./mimap_d -r rules.txt -n counter $@  &
-sleep 2
-vmpk &
 sleep 2
 
 CLIENT=$(aconnect -l | awk '/counter/ {print $2;exit}')
 INPORT=$(aconnect -l | awk '/counter_in/ {print $1;exit}')
 OUTPORT=$(aconnect -l | awk '/counter_out/ {print $1;exit}')
-MIDIKEYS=$(aconnect -l | awk '/VMPK Output/ {print $2;exit}')
+
 echo aconnect ${MIDIKEYS}0 ${CLIENT}${INPORT} 
 aconnect ${MIDIKEYS}0 ${CLIENT}${INPORT}  || exit 1
 
