0e763ab5ef80667cf56a773013d27d8cea489fdf
slmnv5 <slmnv5@gmail.com>
2022-10-23 13:01:00 -0400

    1

--- src/test_2.cpp
+++ src/test_2.cpp
@@ -14,11 +14,11 @@ TEST_CASE("Test RuleMapper 1", "[all]") {
 	MidiEvent e1("c,2,2,3"), e2("n,5,5,11"), e3("n,2,25,11");
 	SECTION("Section to string 1") {
 		REQUIRE(r1.getSize() == 3);
-		string s0 = r1.getRule(0).toString();
+		std::string s0 = r1.getRule(0).toString();
 		REQUIRE(s0 == "n,0:15,0:20,10:127=n,2:2,22:22,22:22=p");
-		string s1 = r1.getRule(1).toString();
+		std::string s1 = r1.getRule(1).toString();
 		REQUIRE(s1 == "n,0:15,0:127,0:127=n,2:2,122:122,0:127=p");
-		string s2 = r1.getRule(2).toString();
+		std::string s2 = r1.getRule(2).toString();
 		REQUIRE(s2 == "c,0:15,0:127,0:127=n,2:2,122:122,0:0=p");
 
 		REQUIRE(r1.findMatchingRule(e1, 0) == 2);
@@ -35,7 +35,7 @@ TEST_CASE("Test RuleMapper 2", "[all]") {
 	MidiEvent e1("n,2,2,3"), e2("n,5,5,11"), e3("c,2,25,11");
 	SECTION("Section to string 1") {
 		REQUIRE(r1.getSize() == 1);
-		string s0 = r1.getRule(0).toString();
+		std::string s0 = r1.getRule(0).toString();
 		REQUIRE(s0 == "n,0:15,0:127,0:127=n,0:15,0:127,0:0=p");
 
 		REQUIRE(r1.findMatchingRule(e1, 0) == 0);
